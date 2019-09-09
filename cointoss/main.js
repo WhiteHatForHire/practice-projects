b47d1323f693a7bd0da612ce8ce49e02e327720f
whitehatforhire <stephensm2011@gmail.com>
2019-09-09 13:38:52 +0530

    Added formatting

--- cointoss/main.js
+++ cointoss/main.js
@@ -1,30 +1,30 @@
-const coinArray = [ 'Heads', 'Tails' ];
-const message = document.querySelector('.message');
-const buttons = document.querySelectorAll('button');
-let score = [ 0, 0 ];
+const coinArray = ["Heads", "Tails"];
+const message = document.querySelector(".message");
+const buttons = document.querySelectorAll("button");
+let score = [0, 0];
 
 // adding event listeners
 for (let i = 0; i < buttons.length; i++) {
-	// console.log(buttons[i]);
-	buttons[i].addEventListener('click', tossCoin);
+  // console.log(buttons[i]);
+  buttons[i].addEventListener("click", tossCoin);
 }
 
 function tossCoin(e) {
-	let playerGuess = e.target.innerText;
-	let computerToss = Math.floor(Math.random() * 2);
-	let computerGuess = coinArray[computerToss];
-	console.log(playerGuess, computerGuess);
-	if (playerGuess === computerGuess) {
-		output = 'Player Wins';
-		//win
-		score[0]++;
-	} else {
-		output = 'Computer Wins';
-		//loss
-		score[1]++;
-	}
+  let playerGuess = e.target.innerText;
+  let computerToss = Math.floor(Math.random() * 2);
+  let computerGuess = coinArray[computerToss];
+  console.log(playerGuess, computerGuess);
+  if (playerGuess === computerGuess) {
+    output = "Player Wins";
+    //win
+    score[0]++;
+  } else {
+    output = "Computer Wins";
+    //loss
+    score[1]++;
+  }
 
-	message.innerHTML = `
+  message.innerHTML = `
     Computer selected ${computerGuess}
     <br>
     Player selected ${playerGuess}
